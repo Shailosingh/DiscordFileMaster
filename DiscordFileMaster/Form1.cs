a026d2bc1de5d0ed30c333ef56fe3e8d30625d04
Shailendra outlook <computerface56@hotmail.com>
2021-10-16 15:22:20 -0400

    Version 1.03: Added comments in Form1 to make things clearer and fixed bug where About page was allowed to open more than once.

--- DiscordFileMaster/Form1.cs
+++ DiscordFileMaster/Form1.cs
@@ -12,12 +12,14 @@ namespace DiscordFileMaster
 {
     public partial class Form1 : Form
     {
-        //Form Trackers
+        //Form Trackers (Used to ensure only one instance of each form can exist)
         FileJoiner joinerForm = null;
         FileSplitter splitterForm = null;
+        About aboutForm = null;
 
         public Form1()
         {
+            //Makes sure window is at fixed size
             InitializeComponent();
             this.MinimumSize = this.Size;
             this.MaximumSize = this.Size;
@@ -32,28 +34,37 @@ namespace DiscordFileMaster
 
         private void about_Click(object sender, EventArgs e)
         {
-            About aboutForm = new About();
+            //Makes sure window is at fixed size
+            aboutForm = new About();
             aboutForm.MinimumSize = aboutForm.Size;
             aboutForm.MaximumSize = aboutForm.Size;
             aboutForm.MaximizeBox = false;
-            aboutForm.Show();
+
+            //Show Dialog ensures it is always a Modal Window (Over the other forms and makes other forms inaccessible)
+            aboutForm.ShowDialog();
         }
 
         private void fileSplitter_Click(object sender, EventArgs e)
         {
+            //Makes sure window is at fixed size
             splitterForm = new FileSplitter();
             splitterForm.MinimumSize = splitterForm.Size;
             splitterForm.MaximumSize = splitterForm.Size;
             splitterForm.MaximizeBox = false;
+
+            //Show Dialog ensures it is always a Modal Window (Over the other forms and makes other forms inaccessible)
             splitterForm.ShowDialog();
         }
 
         private void fileJoin_Click(object sender, EventArgs e)
         {
+            //Makes sure window is at fixed size
             joinerForm = new FileJoiner();
             joinerForm.MinimumSize = joinerForm.Size;
             joinerForm.MaximumSize = joinerForm.Size;
             joinerForm.MaximizeBox = false;
+
+            //Show Dialog ensures it is always a Modal Window (Over the other forms and makes other forms inaccessible)
             joinerForm.ShowDialog();
         }
     }
